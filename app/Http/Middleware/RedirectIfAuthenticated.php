03c9e2b4cf7ecd40c6be330cccac798b116e829e
Fedi Hamdi <fedihamdi97@gmail.com>
2021-07-26 16:50:12 +0100

    fix login redirection

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,7 +20,7 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check()) {
             // return redirect(RouteServiceProvider::HOME);
-        return '/collab';
+        return $next($request);
         }
 
         return $next($request);
