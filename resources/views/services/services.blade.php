959deafa5366121fb8a0e4104c9566704a1106a7
Fedi Hamdi <fedihamdi97@gmail.com>
2021-07-29 01:57:39 +0100

    front end fix

--- resources/views/services/services.blade.php
+++ resources/views/services/services.blade.php
@@ -52,6 +52,6 @@
   </section>
 
  <script src="{{ asset('js/dashboard.js') }}"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+ <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
