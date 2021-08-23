dc412fd3e4e10d3ce2809e5952cabf0a65b721aa
Fedi Hamdi <60851574+fedihamdi7@users.noreply.github.com>
2021-08-23 17:02:35 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role')->default('Collaborator');
+            $table->string('role')->default('Admin');
             $table->date('birth')->nullable();
             $table->string('image')->default('default_profile_image.jpg');
             $table->string('phone')->nullable();
